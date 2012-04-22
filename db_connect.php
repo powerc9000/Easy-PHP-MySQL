138b50656ec71eb1d71102f9c81fe843d8acf4cd
powerc9000 <clay_murray@cmwebadventures.com>
2012-04-22 17:03:03 -0600

    A better way to do db connections

--- db_connect.php
+++ db_connect.php
@@ -1,23 +1,13 @@
 <?php
 class Connection {
 	
-	//Add as many databases as you would like.  See ReadMe for naming conventions.
-	public $db1 = array(
-		"host" => "HOSTNAME",
-		"user" => "USERNAME",
-		"pw" => "PASSWORD",
-		"db_name" => "DATABASE_NAME"
-	);	
-	//If you are only using one database, you may delete the following six lines
-	public $db2 = array(
-		"host" => "HOSTNAME",
-		"user" => "USERNAME",
-		"pw" => "PASSWORD",
-		"db_name" => "DATABASE_NAME"
-	);
+	//this way they never have to change the class only give details makes it easier
+    public function __construct($host,$user,$pw,$db){
+    	$this->$db = array("host"=>$host,"user"=>$user,"pw"=>$pw,"db_name"=>$db);
+    }
 	
 	//You don't have to change anything here :)
-    public function query($db,$query) {
+    public function query($query) {
 		$i = $this->$db;
 		$hidubd = mysql_connect($i['host'], $i['user'], $i['pw']) or die(mysql_error());
 		$connect = mysql_select_db($i['db_name'], $hidubd) or die(mysql_error());
