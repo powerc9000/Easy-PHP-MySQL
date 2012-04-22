f054c280cca856f0726500d5ad0f209589dd5247
powerc9000 <clay_murray@cmwebadventures.com>
2012-04-22 17:38:31 -0600

    Easier time doing authentication plus the ability to fetch your data

--- db_connect.php
+++ db_connect.php
@@ -1,29 +1,27 @@
 <?php
 class Connection {
-	
-	//Add as many databases as you would like.  See ReadMe for naming conventions.
-	public $db1 = array(
-		"host" => "HOSTNAME",
-		"user" => "USERNAME",
-		"pw" => "PASSWORD",
-		"db_name" => "DATABASE_NAME"
-	);	
-	//If you are only using one database, you may delete the following six lines
-	public $db2 = array(
-		"host" => "HOSTNAME",
-		"user" => "USERNAME",
-		"pw" => "PASSWORD",
-		"db_name" => "DATABASE_NAME"
-	);
-	
+
+	public function __construct($host,$user,$password,$db){
+		$this->db = array("host"=>$host,"user"=>$user,"pw"=>$password,"db_name"=>$db);
+	}
+
 	//You don't have to change anything here :)
-    public function query($db,$query) {
-		$i = $this->$db;
+    public function query($query) {
+		$i = $this->db;
 		$hidubd = mysql_connect($i['host'], $i['user'], $i['pw']) or die(mysql_error());
 		$connect = mysql_select_db($i['db_name'], $hidubd) or die(mysql_error());
 		$result = mysql_query($query, $hidubd) or die(mysql_error());
+		$this->result = $result;
 		
-		return $result;
-    }
+	}
+	public function fetch(){
+		$rows = mysql_num_rows($this->result);
+		$data = array();
+		for($i=0;$i<$rows;$i++){
+			$data[$i] = mysql_fetch_assoc($this->result);
+
+		}
+		return $data;
+	}
 }
 ?>
