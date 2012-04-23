d554621bbcd7a39ae2a9d696f9887dc13953a063
powerc9000 <clay_murray@cmwebadventures.com>
2012-04-23 15:28:10 -0600

    Fixed some syntax

--- connections.php
+++ connections.php
@@ -1,10 +1,10 @@
 <?
  $connections = array(
   "db1" => array(
-    "host" => "localhost",
-    "user" => "root",
-    "pw" => "root",
-    "db_name" => "shutttr"
+    "host" => "HOSTNAME",
+    "user" => "USERNAME",
+    "pw" => "PASSWORD",
+    "db_name" => "DATABASE_NAME"
   ),
   "db2" => array(
     "host" => "HOSTNAME",
